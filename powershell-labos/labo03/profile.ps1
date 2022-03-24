7362a69ff768edc8c55edb7dbbf09c8ab8d1de4c
Divine Matondo <divine.matondo@student.ap.be>
2022-03-24 13:54:00 +0100

    update

--- powershell-labos/labo03/profile.ps1
+++ powershell-labos/labo03/profile.ps1
@@ -1,1 +1,8 @@
 Get-Date
+$prosrc = 'C:\Users\Divin\aboutme\OS-Scripting-labo2\powershell-labos\labo03'
+Set-Location $prosrc
+git diff --quiet profile.ps1
+if ( $LASTEXITCODE -eq 0){
+if((Get-Item $profile).LastWriteTime -lt (Get-Item $prosrc\profile.ps1).LastWriteTime){
+Copy-Item $prosrc\profile.ps1 $profile -Verbose 
+.$profile}}
